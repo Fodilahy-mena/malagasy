783f7c906c52d561f4677e98d9c9b94c8c39ba6c
student <jerome.emi@onja.org>
2021-04-21 17:37:16 +0300

    added another component as a trail

---
+++ src/components/MyComponent/MyComponent.js
@@ -0,0 +1,14 @@
+// components/Task.js
+import * as React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+// import { styles } from '../constants/globalStyles';
+
+export default function Example({label}) {
+
+  console.log(label);
+  return (
+    <SafeAreaView >
+      <Text>{label}</Text>
+    </SafeAreaView>
+  );
+}
