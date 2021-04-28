b158760e9dcfac1f5f3a0fa4d7ef746a95668db0
student <jerome.emi@onja.org>
2021-04-28 18:27:23 +0300

    added resuffle button story and deleted unnecessary stories in next button component, also make the button as a controlled component

--- src/components/NextButton/NextButton.stories.js
+++ src/components/NextButton/NextButton.stories.js
@@ -1,53 +1,33 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
+import {action} from '@storybook/addon-actions';
+import {text} from '@storybook/addon-knobs';
+import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import {View} from 'react-native';
 import Button from './NextButton';
 
-storiesOf('Buttons', module)
+storiesOf('Next Button', module)
   .addDecorator(story => <View style={{padding: 23}}>{story()}</View>)
   .add('Next button', () => (
-    <Button isDisabled={false} onPress={action('clicked-text')}>
-      <Text style={
-        {color: '#FFFFFF', fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontWeight: '600',
-        fontSize: 16,
-        textAlign: 'center',
-        lineHeight: 19,}}>Next</Text>
-    </Button>
-  ))
-  .add('Next button long text', () => (
-    <Button isDisabled={false} onPress={action('clicked-text')}>
-      <Text style={
-        {color: '#FFFFFF', fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontWeight: '600',
-        fontSize: 16,
-        textAlign: 'center',
-        lineHeight: 19,}}>Next phrase</Text>
-    </Button>
+    <Button
+      isDisabled={false}
+      textColor="#FFFFFF"
+      text="Next"
+      onPress={action('clicked-text')}
+    />
   ))
   .add('Add button', () => (
-    <Button isDisabled={true} onPress={action('clicked-text')}>
-      <Text style={
-        {color: '#06B6D4', fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontWeight: '600',
-        fontSize: 16,
-        textAlign: 'center',
-        lineHeight: 19,}}>Add</Text>
-    </Button>
+    <Button
+      isDisabled={true}
+      textColor="#06B6D4"
+      text="Add"
+      onPress={action('clicked-text')}
+    />
   ))
-  .add('Add button long text', () => (
-    <Button isDisabled={true} onPress={action('clicked-text')}>
-      <Text style={
-        {color: '#06B6D4', fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontWeight: '600',
-        fontSize: 16,
-        textAlign: 'center',
-        lineHeight: 19,}}>Add a phrase</Text>
-    </Button>
-  ))
+  .add('Reshuffle button', () => (
+    <Button
+      isDisabled={false}
+      textColor="#FFFFFF"
+      text="Reshuffle"
+      onPress={action('clicked-text')}
+    />
+  ));
