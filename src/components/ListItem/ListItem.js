8d25263b88963d0ec8ec39e3584824500b603050
student <jerome.emi@onja.org>
2021-04-28 12:52:29 +0300

    finished ListItem component with stories and displaying the items

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import ActionButton from '../ActionButton/ActionButton';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  SectionList,
+} from 'react-native';
 
-export default function SectionHeading({text}) {
+export default function ListItem({data, text, iconName, iconType, color}) {
+  function onRowPressLearn() {
+    console.log('Pressed to learn screen');
+  }
+
+  function onRowPressAnswer() {
+    console.log('Pressed to check the answer');
+  }
+
   return (
     <SafeAreaView>
-      <Text style={styles.text}>{text}</Text>
+      <SectionList
+        sections={[{data: data}]}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={styles.item}
+            onPress={text === 'Learn' ? onRowPressLearn : onRowPressAnswer}>
+            <View>
+              <Text style={styles.text}>{item.name}</Text>
+            </View>
+            <ActionButton
+              onPress={text === 'Learn' ? onRowPressLearn : onRowPressAnswer}
+              text={text}
+              color={color}
+              iconType={iconType}
+              iconName={iconName}
+            />
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    textAlignVertical: 'center',
+    paddingLeft: 16,
+    paddingRight: 20,
+    paddingVertical: 17,
+  },
   text: {
     fontFamily: 'Inter',
     fontStyle: 'normal',
--- src/components/ListItem/ListItem.stories.js
+++ src/components/ListItem/ListItem.stories.js
@@ -3,8 +3,27 @@ import {View} from 'react-native';
 
 import {storiesOf} from '@storybook/react-native';
 
+import list from './list';
+import answers from './answers';
 import ListItem from './ListItem';
 
 storiesOf('List Item', module)
   .addDecorator(story => <View>{story()}</View>)
-  .add('Item', () => <ListItem text={'At the restaurant'} />);
+  .add('Item category', () => (
+    <ListItem
+      data={list}
+      text={'Learn'}
+      color="#06B6D4"
+      iconType="material-community"
+      iconName="arrow-right"
+    />
+  ))
+  .add('Item answer', () => (
+    <ListItem
+      data={answers}
+      text={'Pick'}
+      color="#06B6D4"
+      iconType="material-community"
+      iconName="arrow-right"
+    />
+  ));
