07db90547b2ee4eb68f5ccfa929b980a684f4ca3
student <jerome.emi@onja.org>
2021-04-30 11:01:29 +0300

    finishing small part of home screen by displaying facke data

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import {action} from '@storybook/addon-actions';
-import {View} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  KeyboardAvoidingView,
+} from 'react-native';
 // import {createStackNavigator} from '@react-navigation/stack';
 // import AsyncStorage from '@react-native-community/async-storage';
 import List from '../components/List/List';
 import list from '../components/ListItem/list';
+import SectionHeading from '../components/SectionHeading/SectionHeading';
 import ToolBar from '../components/ToolBar/ToolBar';
 
 import ToolButton from '../components/ToolButton/ToolButton';
@@ -15,6 +21,25 @@ import CheckAllIcon from '../components/ToolButton/assets/check-all.svg';
 import BackIcon from '../components/ToolButton/assets/back.svg';
 import ModeIcon from '../components/ToolButton/assets/mode.svg';
 
+function Switcher() {
+  const [firstLanguage, setFirstLanguage] = useState(true);
+  function switchLanguage() {
+    setFirstLanguage(!firstLanguage);
+  }
+
+  return (
+    <LanguageSwitcher
+      firstLanguage={firstLanguage}
+      LeftText="EN"
+      RightText="MA"
+      color="#FFFFFF"
+      iconType=""
+      iconName="swap-horiz"
+      onPress={switchLanguage}
+      iconSize={24}
+    />
+  );
+}
 export default ({navigation}) => {
   //   if (loading) {
   //     return (
@@ -25,29 +50,63 @@ export default ({navigation}) => {
   //   }
 
   return (
-    <View>
-      <ToolBar
-        button={
-          <ToolButton onPress={action('clicked-add-button')}>
-            <AddIcon width={24} height={24} fill="#FFFFFF" />
-          </ToolButton>
-        }
-      />
-      <ToolBar
-        button={
-          <ToolButton onPress={action('clicked-add-button')}>
-            <AddIcon width={24} height={24} fill="#FFFFFF" />
-          </ToolButton>
-        }
-      />
-      <List
-        data={list}
-        text={'Learn'}
-        color="#06B6D4"
-        iconType="material-community"
-        iconName="arrow-right"
-        navigation={navigation}
-      />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+        <View style={{paddingHorizontal: 35, paddingVertical: 23}}>
+          <View style={styles.header}>
+            <ToolBar
+              button={
+                <ToolButton onPress={action('clicked-add-button')}>
+                  <AddIcon width={24} height={24} fill="#FFFFFF" />
+                </ToolButton>
+              }
+            />
+            <ToolBar button={<Switcher />} />
+            <ToolBar
+              button={
+                <ToolButton onPress={action('clicked-add-button')}>
+                  <CheckIcon width={24} height={24} fill="#FFFFFF" />
+                </ToolButton>
+              }
+            />
+            <ToolBar
+              button={
+                <ToolButton onPress={action('clicked-add-button')}>
+                  <CheckAllIcon width={24} height={24} fill="#FFFFFF" />
+                </ToolButton>
+              }
+            />
+            <ToolBar
+              button={
+                <ToolButton onPress={action('clicked-add-button')}>
+                  <ModeIcon width={24} height={24} fill="#FFFFFF" />
+                </ToolButton>
+              }
+            />
+          </View>
+          <View style={styles.heading}>
+            <SectionHeading text="Select a category:" />
+          </View>
+          <List
+            data={list}
+            text={'Learn'}
+            color="#06B6D4"
+            iconType="material-community"
+            iconName="arrow-right"
+            navigation={navigation}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    paddingBottom: 56,
+  },
+  heading: {
+    paddingBottom: 15,
+  },
+});
