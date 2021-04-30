aca440e071cf84127c90f290a5e6b68538f2e7cd
student <jerome.emi@onja.org>
2021-04-30 09:53:40 +0300

    test tool bar on home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,19 @@
 import React, {useState, useEffect} from 'react';
+import {action} from '@storybook/addon-actions';
 import {View} from 'react-native';
 // import {createStackNavigator} from '@react-navigation/stack';
 // import AsyncStorage from '@react-native-community/async-storage';
 import List from '../components/List/List';
 import list from '../components/ListItem/list';
+import ToolBar from '../components/ToolBar/ToolBar';
+
+import ToolButton from '../components/ToolButton/ToolButton';
+import LanguageSwitcher from '../components/LanguageSwitcher/LanguageSwitcher';
+import AddIcon from '../components/ToolButton/assets/add.svg';
+import CheckIcon from '../components/ToolButton/assets/check.svg';
+import CheckAllIcon from '../components/ToolButton/assets/check-all.svg';
+import BackIcon from '../components/ToolButton/assets/back.svg';
+import ModeIcon from '../components/ToolButton/assets/mode.svg';
 
 export default ({navigation}) => {
   //   if (loading) {
@@ -16,6 +26,20 @@ export default ({navigation}) => {
 
   return (
     <View>
+      <ToolBar
+        button={
+          <ToolButton onPress={action('clicked-add-button')}>
+            <AddIcon width={24} height={24} fill="#FFFFFF" />
+          </ToolButton>
+        }
+      />
+      <ToolBar
+        button={
+          <ToolButton onPress={action('clicked-add-button')}>
+            <AddIcon width={24} height={24} fill="#FFFFFF" />
+          </ToolButton>
+        }
+      />
       <List
         data={list}
         text={'Learn'}
