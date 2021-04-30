59604a4e2100c196ace6d63a7c16cca641262695
student <jerome.emi@onja.org>
2021-04-30 09:56:56 +0300

    modified tool bar

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -3,17 +3,10 @@ import PropTypes from 'prop-types';
 import {TouchableHighlight, StyleSheet, View} from 'react-native';
 
 export default function ToolBar({button}) {
-  return (
-    <View style={styles.header}>
-      <View style={styles.button}>{button}</View>
-    </View>
-  );
+  return <View style={styles.button}>{button}</View>;
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-  },
   button: {
     marginRight: 10,
   },
