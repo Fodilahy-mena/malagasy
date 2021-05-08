32a0eb22e36bbf444f4d55ab5b89d10be27258a0
student <jerome.emi@onja.org>
2021-05-08 11:01:31 +0300

    deleted commented state

--- src/state.js
+++ src/state.js
@@ -2,7 +2,4 @@ export default {
   categories: [],
   phrases: [],
   randomPhrase: {},
-  // solutionButtonColor: '#06B6D4',
-  // solutionButtonIconName: 'arrow-right',
-  // solutionButtonIconType: 'material-community',
 };
